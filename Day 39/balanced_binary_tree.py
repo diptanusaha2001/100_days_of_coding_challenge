1f2f0c13a066c85ec65a46220a923a4ba9fb6bda
Diptanu Saha <diptanusaha2001@gmail.com>
2023-06-04 14:56:43 +0530

    Create balanced_binary_tree.py

---
+++ Day 39/balanced_binary_tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+        if root.left is None and root.right is None:
+            return True
+        def compareH(node):
+            if node is None:
+                return 1
+            dr = compareH(node.right)
+            if dr == 0:
+                return 0
+            dl = compareH(node.left)
+            if dl == 0:
+                return 0
+            if abs(dr-dl) > 1:
+                return 0
+            return max(dr,dl)+1
+        return compareH(root) != 0
