0e7f0cc1418d1f53769277f1c843daa0b2b1b75e
Diptanu Saha <diptanusaha2001@gmail.com>
2023-07-01 07:25:44 +0530

    Update fair_distribution_of_cookies.py

--- Day 66/fair_distribution_of_cookies.py
+++ Day 66/fair_distribution_of_cookies.py
@@ -1,21 +1,19 @@
+def distributeCookies(cookies, k):
+    children = [0] * k
+    result = float('inf')
 
-class Solution:
-    def distributeCookies(self, cookies: List[int], k: int) -> int:
-        children = [0] * k
-        result = float('inf')
+    def recurse(i, children):
+        if i == len(cookies):
+            nonlocal result
+            result = min(result, max(children))
+            return
 
-        def recurse(i, children):
-            if i == len(cookies):
-                nonlocal result
-                result = min(result, max(children))
-                return
+        cookie_size = cookies[i]
+        for child in range(min(i + 1, k)): 
+            children[child] += cookie_size
+            recurse(i + 1, children)
+            children[child] -= cookie_size
 
-            cookie_size = cookies[i]
-            for child in range(min(i + 1, k)): 
-                children[child] += cookie_size
-                recurse(i + 1, children)
-                children[child] -= cookie_size
+    recurse(0, children)
 
-        recurse(0, children)
-
-        return result
+    return result
